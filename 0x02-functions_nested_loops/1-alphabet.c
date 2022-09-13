dd2c6d5a764ff45e46126d57288e18e5874745ae
BoladeOlalekan <lekanseyibolade@gmail.com>
2022-09-13 15:23:35 -0700

    Updated file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet, in lowercase, followed by a new line.
+ * print_alphabet - Prints the alphabet, in lowercase, followed by a new line.
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 	char alph1;
 
