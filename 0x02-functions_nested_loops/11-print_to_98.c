4f4364376808937e10751947bb2a64e1da5d1dc8
BoladeOlalekan <lekanseyibolade@gmail.com>
2022-09-14 14:03:20 -0700

    Updated file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,8 @@ void print_to_98(int n)
 	for (n = 0; n <= 98; n++)
 	{
 		_putchar(n);
-		_putchar(', ');
+		_putchar(',');
+		_putchar(' ');
 
 		if (n == 98)
 		{
@@ -19,17 +20,20 @@ void print_to_98(int n)
 		for (n = 111; n >= 98; n--)
 		{
 			_putchar(n);
-			_putchar(', ');
+			_putchar(',');
+			_putchar(' ');
 		}
 		for (n = 81; n <= 98; n++)
 		{
 			_putchar(n);
-			_puutchar(', ');
+			_puutchar(',');
+			_putchar(' );
 		}
 		for (n = -10; n <= 98; n++)
 		{
 			_putchar(n);
-			_putchar(', ');
+			_putchar(',');
+			_putchar(' ');
 		}
 		_putchar('\n');
 	}
