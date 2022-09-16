0a8a55136a407529220c71515d94a46d7434579a
BoladeOlalekan <lekanseyibolade@gmail.com>
2022-09-16 03:24:09 -0700

    Updated file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,11 @@ void print_most_numbers(void)
 {
 	int n = 0;
 
-	for (n < 10; n++)
+	while (n < 10)
 	{
 		if (n != 2 && n != 4)
-		{
-			_putchar(n + '0');
-		}
+			_putchar(n);
+		n++;
 	}
 	_putchar("\n");
 }
