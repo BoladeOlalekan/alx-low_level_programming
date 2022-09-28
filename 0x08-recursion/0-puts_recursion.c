40ed905636d496a6d0924201b7b9e099a03f5399
BoladeOlalekan <lekanseyibolade@gmail.com>
2022-09-28 09:16:24 -0700

    Initial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion - Function that prints a
+ * new string.
+ * @s: String to be printed
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	_putchar("\n");
+}
