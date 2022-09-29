4cf10ae927f0a6f2f714a9d63734ea0ad69f4600
BoladeOlalekan <lekanseyibolade@gmail.com>
2022-09-29 09:00:05 -0700

    added task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Function to return
+ * the length of a string
+ * @s: Pointer to the string
+ * Return: Length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int n;
+
+	if (*s > '\0')
+	{
+		n += _strlen_recursion(s + 1) + 1;
+	}
+	return (n);
+}
