ccf3359dc9bf132fbd3be0ea71f7cfbe9f54890f
BoladeOlalekan <lekanseyibolade@gmail.com>
2022-09-29 13:23:18 -0700

    Updated file for task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,7 +8,7 @@
  */
 int factorial(int n)
 {
-	if (n < 1)
+	if (n < 0)
 	{
 		return (-1);
 	}
@@ -16,5 +16,8 @@ int factorial(int n)
 	{
 		return (1);
 	}
+	else
+	{
 	return (n + factorial(n - 1));
+	}
 }
