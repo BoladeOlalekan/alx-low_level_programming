69ae7bb3f0aecefc364e0df603a7a708dd082f40
BoladeOlalekan <lekanseyibolade@gmail.com>
2022-09-29 18:17:10 -0700

    Added task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * is_prime_number - Prints prime number
+ * @n: Integer to be checked
+ * Return: 1 if integer is prime number,
+ * or 0 if integer is not
+ */
+int is_prime_number(int n)
+{
+	if (n / 1 == n && n / n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
